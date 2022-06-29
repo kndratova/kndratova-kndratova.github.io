3f7b3e7066e213060a22da54ecc30c14e9c4da3d
kndratova <90265158+kndratova@users.noreply.github.com>
2022-06-30 00:47:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function jump () {
     }, 1000)
 }
 
-let isAlive = setInterval(function(){
-    let dinoTop = parseInt(window.getComputedStyle(dinosaur).getPropertyValue("top"));
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+// let isAlive = setInterval(function(){
+//     let dinoTop = parseInt(window.getComputedStyle(dinosaur).getPropertyValue("top"));
+//     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
-        alert("GAME OVER!");
-    }
-}, 10)
+//     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+//         alert("GAME OVER!");
+//     }
+// }, 10)
